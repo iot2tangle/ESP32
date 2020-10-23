52902b4ae929eed70fa5f97ebdd0d28e00b2d081
Gustavo Belbruno <gustavobelbruno@gmail.com>
2020-10-22 23:27:56 -0300

    - Added BH1750 - Light Sensor

--- http/main/core/ESP32/sensors/bh1750/bh1750.c
+++ http/main/core/ESP32/sensors/bh1750/bh1750.c
@@ -1,43 +1,77 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "bh1750.h"
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "sdkconfig.h"
+
+#define I2C_PORT_NUMBER 1
+
+#define BH1750_SENSOR_ADDR 0x23   /*!< slave address for BH1750 sensor */
+#define BH1750_CMD_START 0x23   /*!< Operation mode */
+#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
+#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
+#define ACK_VAL 0x0                             /*!< I2C ack value */
+#define NACK_VAL 0x1                            /*!< I2C nack value */
 
 char buffer[100];
 char* s;
 
-int data;
+i2c_port_t i2c_num = I2C_PORT_NUMBER;
 
 bool check_bh1750()
 {
-	// IF BH1750 IS DETECTED  
-	return true;
+	int ret;
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, BH1750_SENSOR_ADDR << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, BH1750_CMD_START, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    if (ret == ESP_OK)
+    	return true;
+    else
+    	return false;
 }
 
-void init_bh1750(bool ft)
+void init_bh1750(bool _ft)
 {
-	; // INIT BH1750
-} 
-
+	;// Not necesary in this case
+}
+ 
 void print_bh1750()
 {   
     if (check_bh1750())
-	printf("BH1750 : OK");
+		printf("BH1750 : OK");
     else
-	printf("BH1750 : Not detected (Light sensor)");
+		printf("BH1750 : Not detected (Light sensor)");
 }
-	
+
 char* get_bh1750()
 {
+    uint8_t data_h, data_l;
+    float d;
+    
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, BH1750_SENSOR_ADDR << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, BH1750_CMD_START, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    vTaskDelay(30 / portTICK_RATE_MS);
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, BH1750_SENSOR_ADDR << 1 | READ_BIT, ACK_CHECK_EN);
+    i2c_master_read_byte(cmd, &data_h, ACK_VAL);
+    i2c_master_read_byte(cmd, &data_l, NACK_VAL);
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    d = (data_h << 8 | data_l) / 1.2;
+    
     s = " ";
-
-//  READ SENSOR DATA
-    data = 230;
-
-    sprintf(buffer, "%d", data);
-
+    sprintf(buffer, "%.02f", d);
     s=buffer;
-    return s; /* Light */
+    return s;
 }
-
